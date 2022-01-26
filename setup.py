068c9635542f02cd0075c0a6d2b1cd3203a79c41
Luka Cehovin Zajc <luka.cehovin@gmail.com>
2022-01-26 15:05:56 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if isfile(join(this_directory, "requirements.txt")):
         install_requires = f.readlines()
 
 setup(name='attributee',
-    version="0.1.4",
+    version="0.1.5",
     description='Declarative object initialization library',
     long_description=long_description,
     long_description_content_type='text/markdown',
